50ace2c7e0eab0fed983f1004e80392f1ef3c932
Zryte <>
2023-02-15 11:54:00 +0700

    update Package.swift for v2.34.2

--- Package.swift
+++ Package.swift
@@ -13,18 +13,18 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmityUIKit",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/AmityUIKit.xcframework.zip",
-            checksum: "1e3f2df0eaae894c73f6d5729f79bd4417ed800c79dbe909a54cee9968b22502"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.34.2/AmityUIKit.xcframework.zip",
+            checksum: "c757a82431cab9449e06e0f9a685ee30665c73056e8b53b4c95f96397a9ac7f9"
         ),
         .binaryTarget(
             name: "AmitySDK",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/AmitySDK.xcframework.zip",
-            checksum: "232ee5f35491e3f4c60909d48c0159edea78e57a40bb3b0a7539cfc63ba8d3fb"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.34.2/AmitySDK.xcframework.zip",
+            checksum: "b41498fad3df89cf1856bf1faa6718cbdbfd1b6641d4a8b299cf54373c180dd8"
         ),
         .binaryTarget(
             name: "Realm",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/Realm.xcframework.zip",
-            checksum: "cb1c29cb221ee4b71de8ccbc581ab9c1c3047a49d9b2433522268c601d8284b5"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/2.34.2/Realm.xcframework.zip",
+            checksum: "68580d5ed2f7a0f7770dcf1c3f28d0cc50fe2cafdb62a268f6b0c3e008b42967"
         )
     ]
 )
