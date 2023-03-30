d6e56787d403f9bcf9a3b608f752e4f8430c6f86
Zryte <>
2023-03-30 17:12:14 +0700

    update Package.swift for v3.0.0

--- Package.swift
+++ Package.swift
@@ -8,23 +8,28 @@ let package = Package(
     products: [
         .library(
             name: "AmityUIKit",
-            targets: ["AmityUIKit", "AmitySDK", "Realm"])
+            targets: ["AmityUIKit", "AmitySDK", "Realm", "RealmSwift"])
     ],
     targets: [
         .binaryTarget(
             name: "AmityUIKit",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/AmityUIKit.xcframework.zip",
-            checksum: "1e3f2df0eaae894c73f6d5729f79bd4417ed800c79dbe909a54cee9968b22502"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.0.0/AmityUIKit.xcframework.zip",
+            checksum: "b2d78e35f17a48c512d618e5707824869c695bad9a3e13fa0e31a6b5c755fbd4"
         ),
         .binaryTarget(
             name: "AmitySDK",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/AmitySDK.xcframework.zip",
-            checksum: "232ee5f35491e3f4c60909d48c0159edea78e57a40bb3b0a7539cfc63ba8d3fb"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.0.0/AmitySDK.xcframework.zip",
+            checksum: "8e65f811ee50bfa97053786457fbe642f2ea57f995b2277e26792bb2260b8157"
         ),
         .binaryTarget(
             name: "Realm",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/Realm.xcframework.zip",
-            checksum: "cb1c29cb221ee4b71de8ccbc581ab9c1c3047a49d9b2433522268c601d8284b5"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.0.0/Realm.xcframework.zip",
+            checksum: "7309207506b1680546907505c07188f355b02e76fcd6d977ed494aee812cd103"
+        ),
+        .binaryTarget(
+            name: "RealmSwift",
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.0.0/RealmSwift.xcframework.zip",
+            checksum: "3929b6b355d6b72b6d0b98a02ca24122aa10147169c46a20b022147714adef00"
         )
     ]
 )
