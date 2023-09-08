ccafbae68e4182254b945f470a0cc880d9db809d
Zayar <zayariosdev@icloud.com>
2023-09-08 18:39:25 +0700

    update Package.swift for v3.11.0

--- Package.swift
+++ Package.swift
@@ -8,23 +8,28 @@ let package = Package(
     products: [
         .library(
             name: "AmityUIKit",
-            targets: ["AmityUIKit", "AmitySDK", "Realm"])
+            targets: ["AmityUIKit", "AmitySDK", "Realm", "RealmSwift"])
     ],
     targets: [
         .binaryTarget(
             name: "AmityUIKit",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/AmityUIKit.xcframework.zip",
-            checksum: "1e3f2df0eaae894c73f6d5729f79bd4417ed800c79dbe909a54cee9968b22502"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.11.0/AmityUIKit.xcframework.zip",
+            checksum: "428ba9b578dc6c16cf86ed7e1752b94cb535d2b167bbb057f9a31d585c492080"
         ),
         .binaryTarget(
             name: "AmitySDK",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/AmitySDK.xcframework.zip",
-            checksum: "232ee5f35491e3f4c60909d48c0159edea78e57a40bb3b0a7539cfc63ba8d3fb"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.11.0/AmitySDK.xcframework.zip",
+            checksum: "d736c6b6cb1607957e0d280e87b9d1b8f77238d7ab944c9448cc6a40bbbd2db4"
         ),
         .binaryTarget(
             name: "Realm",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/Realm.xcframework.zip",
-            checksum: "cb1c29cb221ee4b71de8ccbc581ab9c1c3047a49d9b2433522268c601d8284b5"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.11.0/Realm.xcframework.zip",
+            checksum: "a1e9b8a76cf75a848a85c5b718febb69b3bd074ae5529e0e90203cbe9fbf7496"
+        ),
+        .binaryTarget(
+            name: "RealmSwift",
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.11.0/RealmSwift.xcframework.zip",
+            checksum: "c91d401c8ee8fe2cf91c1744c24d5437fe04e01c275120f2cebdefbd461cafa0"
         )
     ]
 )
