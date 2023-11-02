1216d47881f2962abb186525199262779e2ff5a0
Zryte <Zryte@gmail.com>
2023-11-02 19:33:07 +0700

    update Package.swift for v3.15.0

--- Package.swift
+++ Package.swift
@@ -8,23 +8,28 @@ let package = Package(
     products: [
         .library(
             name: "AmityUIKit",
-            targets: ["AmityUIKit", "AmitySDK", "Realm"])
+            targets: ["AmityUIKit", "AmitySDK", "Realm", "RealmSwift"])
     ],
     targets: [
         .binaryTarget(
             name: "AmityUIKit",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/AmityUIKit.xcframework.zip",
-            checksum: "1e3f2df0eaae894c73f6d5729f79bd4417ed800c79dbe909a54cee9968b22502"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.15.0/AmityUIKit.xcframework.zip",
+            checksum: "442519572201766ac2f23399c8b9f8373d8557218e0f1ee187e6d6a841634c8b"
         ),
         .binaryTarget(
             name: "AmitySDK",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/AmitySDK.xcframework.zip",
-            checksum: "232ee5f35491e3f4c60909d48c0159edea78e57a40bb3b0a7539cfc63ba8d3fb"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.15.0/AmitySDK.xcframework.zip",
+            checksum: "2c1d0f00cd5a7fb09193b7667cd6e864a6692168e895efd3094c58a5a4580177"
         ),
         .binaryTarget(
             name: "Realm",
-            url: "https://s3-ap-southeast-1.amazonaws.com/ekosdk-release/ios-uikit-frameworks/2.12.0/Realm.xcframework.zip",
-            checksum: "cb1c29cb221ee4b71de8ccbc581ab9c1c3047a49d9b2433522268c601d8284b5"
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.15.0/Realm.xcframework.zip",
+            checksum: "341867d5d8cf04ec40fefb7de30ff7cb903289f03a782e1e93457a5d847a5d93"
+        ),
+        .binaryTarget(
+            name: "RealmSwift",
+            url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.15.0/RealmSwift.xcframework.zip",
+            checksum: "56289faaea2a2b64c53b1b41716c13a6c30000bf7eefee1cb70526e033a08c78"
         )
     ]
 )
